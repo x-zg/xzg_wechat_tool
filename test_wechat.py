e6bdb2f331b4a5787ccce895dac69d06eac7a88e
xu-zhiguo <50972704+xu-zhiguo@users.noreply.github.com>
2026-03-12 09:55:52 +0800

    fix

--- test_wechat.py
+++ test_wechat.py
@@ -6,7 +6,7 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from server import send_message_to_current
+from agent import send_message_to_current
 
 
 def test_send(message):
